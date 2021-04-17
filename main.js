bff4e845211af80f0e53007a2bc5988f297c3c62
Avic Ndugu <avicnotes.com>
2021-04-17 13:46:01 +0300

    added columns using javascript

--- main.js
+++ main.js
@@ -14,16 +14,62 @@ async function getProducts() {
 // Do something with the data
 async function renderProducts() {
   let products = await getProducts();
-  console.log(products);
+  // console.log(products);
   let html = '';
   products.forEach(product => {
-      let productHolder = `<div class="product" id="${product.no}">
-      											<p>${product.no}</p>
-                          	<p>${product.name}</p>
-                      		</div>`;
-      html += productHolder;
+    let productHolder = `<div class="column">
+	<!-- Card -->
+	<div class="columns is-mobile">
+		<div class="column is-one-fifth">
+			<figure>
+				<img src="https://storage.googleapis.com/wineshop-assets/wine-bottles/${product.image}">
+			</figure>
+		</div>
+		<div class="column is-four-fifths">
+      <h2 class="title">${product.no}</h2>
+      <h3 class="subtitle">${product.name}</h3>
+      <div class="columns is-mobile">
+      	<div class="column">
+      		<p>Bottle</p>
+          <p>$ <span>${product.cost.bottle}</span></p>
+          <div class="field is-grouped">
+					  <p class="control">
+					    <input class="input" size="1" type="text" placeholder="">
+					  </p>
+					  <p class="control">
+					    QTY
+					  </p>
+					</div>
+      	</div>
+      	<div class="column">
+      		<p>Case</p>
+          <p>$ <span>${product.cost.case}</span></p>
+          <div class="field is-grouped">
+					  <p class="control">
+					    <input class="input" size="1" type="text" placeholder="">
+					  </p>
+					  <p class="control">
+					    QTY
+					  </p>
+					</div>
+      	</div>
+      </div>
+      <div class="columns is-mobile">
+      	<div class="column">
+      		<button class="button">Details</button>
+      	</div>
+	      <div class="column">
+	      	<button class="button">Add To Cart</button>
+      	</div>
+      </div>
+	  </div>
+	</div>
+	<!-- End of card -->
+</div>`;
+			html += productHolder;
   });
   productsHolder.innerHTML = html;
 }
+
 
 renderProducts();
